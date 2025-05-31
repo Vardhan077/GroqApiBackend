98e192617797519031e111ab1f37f6404addc2a3
Vardhan077 <vardhanmarriwada777@gmail.com>
2025-05-31 20:53:29 +0530

    final

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from agno.agent import Agent
 from agno.models.groq import Groq
 from agno.tools.duckduckgo import DuckDuckGoTools
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import os
 
 load_dotenv()
@@ -35,16 +36,38 @@ app.add_middleware(
     allow_headers=["*"],            # allow all headers
 )
 
+
+class UserRequest(BaseModel):
+    query: str
+
 @app.get("/")
 def root():
     return {"message": "Server running"}
 
+
+# @app.post("/ask")
+# def ask_question(user_input: UserRequest):
+#     try:
+#         response = agent.run(user_input.query)
+#         print("Agent response:", response)
+#         return {"response": response.content}
+#     except Exception as e:
+#         print("Exception:", e)
+#         return {"error": str(e)}
+
+
+from fastapi.responses import StreamingResponse
+
 @app.post("/ask")
-def ask_question():
-    try:
-        response = agent.run("Suggest me some trending insurance plans for a 25-year-old")
-        print("Agent response:", response)
-        return {"response": response.content}
-    except Exception as e:
-        print("Exception:", e)
-        return {"error": str(e)}
+async def ask_question(user_input: UserRequest):
+    async def event_stream():
+        
+        try:
+            response = agent.run(user_input.query)
+            for chunk in response.content.split():
+                yield f"{chunk} "  # Send words as individual chunks
+                
+        except Exception as e:
+            yield f"Error: {str(e)}"
+
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
